225b14aa400d2c2cb87a20e123ca780eccffad23
anna <anna.wallin@imaginecare.com>
2023-03-10 23:27:53 +0100

    First commit

---
+++ SpaceshipParkinglot/ParkingLot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceshipParkinglot;
+
+public class ParkingLot
+{
+    public string Id { get; set; }
+    public int Floor { get; set; }
+    public bool Occupied { get; set; }
+    public string? CarNumberPlate { get; set; }
+    public DateTime? Timpestamp { get; set; }
+}
